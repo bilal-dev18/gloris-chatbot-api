e59cc845bb924fb9eca243214de6e7876402d599
Sheraz Ahmed <sherazahmed93@gmail.com>
2021-06-08 21:18:23 +0500

    Chatbot Mongoose API

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+require('dotenv').config();
+import express from 'express';
+
+import { searchMessage } from './utils/database';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({message: 'The Chatbot server is running!'});
+});
+
+app.post('/chatbot', (req, res) => {
+  console.log(req.body.message);
+  const searchResults = searchMessage(req.body.message);
+  res.json(searchResults);
+});
+
+
+
+app.listen(port, () => {
+  try{
+    return console.log(`server is listening on ${port}`);
+  }
+  catch (err) {
+    return console.error(err);
+  }
+});
---
+++ src/utils/database.ts
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}?retryWrites=true&writeConcern=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export async function searchMessage(message: string) {
+  try {
+    await client.connect();
+    const database = client.db(process.env.DB_NAME);
+    const translations = database.collection(process.env.DB_COLLECTION);
+
+    const cursor = await translations.find({$text: {$search: message}});
+    const resultObject = await cursor.project({
+      name: 1,
+      description: 1,
+      trainingData: 1,
+      reply: 1
+    });
+    await cursor.forEach(console.dir);
+    return resultObject;
+  }catch(err){
+    console.error(err.message);
+  }
+   finally {
+    await client.close();
+  }
+}
